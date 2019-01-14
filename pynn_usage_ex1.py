9380ddc5d10dea44f788b6a5bd9127356061f404
luxizh <1063013131@qq.com>
2019-01-14 22:24:55 +0000

    failed beacause of mpi4py

---
+++ pynn_usage_ex1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 14 20:15:36 2019
+
+@author: luxi
+"""
+#import pyNN
+from pyNN.neuron import *
+#setup(timestep=0.1,min_delay=2.0) 
