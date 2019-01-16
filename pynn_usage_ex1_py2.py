03df364869803cf1e343d92689f6e8c5d2fc84a7
luxizh <1063013131@qq.com>
2019-01-16 11:15:11 +0000

    add example

--- pynn_usage_ex1_py2.py
+++ pynn_usage_ex1_py2.py
@@ -7,5 +7,7 @@ Created on Mon Jan 14 20:15:36 2019
 """
 #import pyNN
 #from pyNN import *
-import pyNN.brian2
+from pyNN.brian import *
+#from brian import *
 setup(timestep=0.1,min_delay=2.0) 
+ifcell=create(IF_cond_exp,{'i_offset':0.11,'tau_refrac':3.0,'v_thresh':-51.0})
